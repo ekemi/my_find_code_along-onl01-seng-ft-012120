f80506bec900ce3226524bb39f5acfee5b5d9dcf
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-25 20:23:11 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,4 +2,17 @@ require 'pry'
 
 def my_find(collection)
 
+  i = 0 
+  
+  while i < collection.lenth 
+    
+   return collection[i]  if  yield(collection[i])
+     
+   end  
+   
+    
+    i = i + 1
+    
+  end 
+  
 end
